fa2ba97531e32c9503c52e14f470478100b92e15
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-20 22:39:26 +0530

    Added TextInput, Button, State Hook and styles

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [ enteredGoal, setEnteredGoal ] = useState('');
+  const [ listGoals, setListGoals ] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setListGoals(listGoals => [...listGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.rootViewStyle}>
+      <View style={styles.inputContainerStyle}>
+        <TextInput 
+        placeholder="Please enter your goal here" 
+        style={styles.textInputStyle} 
+        onChangeText={goalInputHandler}
+        value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ rootViewStyle: {
+  padding: 30
+ },
+ inputContainerStyle: {
+  flexDirection: 'row', 
+  justifyContent: 'space-between', 
+  alignItems: 'center'
+ },
+ textInputStyle: {
+  borderColor: 'black', 
+  borderWidth: 1, 
+  width: '80%'
+ }
 });
